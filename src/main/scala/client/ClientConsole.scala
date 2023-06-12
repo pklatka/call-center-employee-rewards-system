b05eabac62d7f8f9a533b910be1ae690ed2afb6e
Patryk Klatka <patryk.klatka@gmail.com>
2023-06-12 23:16:30 +0200

    Validate user input

--- src/main/scala/client/ClientConsole.scala
+++ src/main/scala/client/ClientConsole.scala
@@ -6,6 +6,7 @@ import streaming.StreamingEntry
 
 import java.util.Properties
 import scala.annotation.tailrec
+import scala.util.Try
 
 object ClientConsole {
   private def help(): Unit = {
@@ -26,11 +27,11 @@ object ClientConsole {
 
     // Prompting for employee data
     println("Enter your ID:")
-    val id = scala.io.StdIn.readLine().toUpperCase
+    val id = validateID("ID")
     println("Enter your first name:")
-    val firstName = scala.io.StdIn.readLine().capitalize
+    val firstName = validateName("first name")
     println("Enter your last name:")
-    val lastName = scala.io.StdIn.readLine().capitalize
+    val lastName = validateName("last name")
 
     help()
 
@@ -43,15 +44,16 @@ object ClientConsole {
           // Enter client data
           // It may be useful when there is a need to persist the order data
           println("Enter clients first name:")
-          val clientFirstName = scala.io.StdIn.readLine().capitalize
+          val clientFirstName = validateName("client first name")
           println("Enter clients last name:")
-          val clientLastName = scala.io.StdIn.readLine().capitalize
+          val clientLastName = validateName("client last name")
           println("Enter clients e-mail address:")
-          val clientEmail = scala.io.StdIn.readLine()
+          val clientEmail = validateEmail("client email")
           println("Enter order description:")
           val orderDescription = scala.io.StdIn.readLine()
           println("Enter order value:")
-          val orderValue = validateInput(scala.io.StdIn.readLine().toDouble, "order value")
+          val orderValue = validateValue("order value")
+          println(orderValue)
 
           // Assign order value to given employee
           val entry = StreamingEntry(id, firstName, lastName, 1, orderValue)
@@ -72,13 +74,57 @@ object ClientConsole {
   }
 
   @tailrec
-  private def validateInput(value: Double, fieldName: String): Double = {
-    if (value >= 0.0) {
-      value
-    } else {
-      println(s"Invalid $fieldName. Please enter a non-negative value.")
-      // Prompt again until a valid input is provided
-      validateInput(scala.io.StdIn.readInt(), fieldName)
+  private def validateValue(fieldName: String): Double = {
+    val userInput = scala.io.StdIn.readLine().trim
+    val value = Try(userInput.toDouble)
+
+    value match {
+      case scala.util.Success(value) if value > 0 =>
+        BigDecimal(value).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+      case _ =>
+        println(s"Invalid $fieldName. Please enter a non-negative value.")
+        validateValue(fieldName)
+    }
+  }
+
+  @tailrec
+  private def validateName(fieldName: String): String = {
+    val userInput = scala.io.StdIn.readLine()
+    val name = userInput.trim().toLowerCase().capitalize.replaceAll("""[\p{Punct}]""", "")
+    val nameRegex = """^[A-Z][a-z]+$""".r
+    name match {
+      case nameRegex() => name
+      case _ =>
+        println(s"Invalid $fieldName. Please enter a valid.")
+        // Prompt again until a valid input is provided
+        validateName(fieldName)
+    }
+  }
+
+  @tailrec
+  private def validateID(fieldName: String): String = {
+    val userInput = scala.io.StdIn.readLine()
+    val id = userInput.trim().toUpperCase().replaceAll("""[\p{Punct}]""", "")
+    val IDRegex = """^[A-Z]+$""".r
+    id match {
+      case IDRegex() => id
+      case _ =>
+        println(s"Invalid $fieldName. Please enter a valid.")
+        // Prompt again until a valid input is provided
+        validateID(fieldName)
+    }
+  }
+
+  @tailrec
+  private def validateEmail(fieldName: String): String = {
+    val email = scala.io.StdIn.readLine()
+    val emailRegex = """^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$""".r
+    email match {
+      case emailRegex() => email
+      case _ =>
+        println(s"Invalid $fieldName. Please enter a valid email address.")
+        // Prompt again until a valid input is provided
+        validateEmail(fieldName)
     }
   }
 }
